f283b8083d8b5f68124545196f3ec05acbf61f43
sunweihaowei <1096187594@qq.com>
2020-08-20 11:29:43 +0800

    多线程编程

---
+++ ExampleMultithreading/src/main/java/com/sunweihao/syn/TestJUC.java
@@ -0,0 +1,20 @@
+package com.sunweihao.syn;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class TestJUC {
+    public static void main(String[] args) {
+        CopyOnWriteArrayList<String> list=new CopyOnWriteArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            new Thread(()->{
+                list.add(Thread.currentThread().getName());
+            }).start();
+        }
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(list.size());
+    }
+}
